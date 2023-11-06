8e1c3362a156aeb2589d0c881f781e450e3f9fee
MatteoSanson <matteo.sanson@gmail.com>
2023-11-06 15:44:46 +0100

    feat: km età e prezzo totale

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 'use strict';
 
 // 1) Tramite prompt chiedo all'utente il numero di km che deve percorrere (ottengo variabile stringa che trasformo in numero e stampo in console).
+const chilometri = prompt("Inserisci il numero di chilometri totali del tuo viaggio");
+const chilometriNumero = parseFloat(chilometri.replace(',','.'));
+console.log ("numero di km viaggio: ", chilometriNumero);
 // 2) Tramite prompt chiedo all'utente la sua età (ottengo variabile stringa che trasformo in numero e stampo in console).
+const eta = prompt("Inserisci la tua età");
+const etaNumero = parseInt(eta);
+console.log ("la tua età: ", etaNumero);
 // 3) Ho una variabile prezzoKm pari a 0,21.
+const prezzoKm = 0.21;
 // 4) Ottengo il prezzoTotale: km * prezzoKm.
+const prezzoTotale = chilometriNumero * prezzoKm;
+console.log ("Il prezzo totale del tuo viaggio è: ", prezzoTotale + " €");
+
 /*
 5) Applico la scontistica con le condizioni:
     - se l'età < 18
